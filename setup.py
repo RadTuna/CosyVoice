898d784eea8f1b39e441bd4dcde66afcb47a3a57
Jaeseung Jeon <wotmd6180@gmail.com>
2025-06-22 02:29:21 +0900

    update requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+import os
+
+# Get the absolute path of requirements.txt
+req_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
+
+# Read requirements.txt safely
+with open(req_path, "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="CosyVoice",
+    version="0.1",
+    packages=find_packages(),
+    package_dir={"CosyVoice": "CosyVoice"},
+    install_requires=requirements,
+)
